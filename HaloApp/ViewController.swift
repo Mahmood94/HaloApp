1b9a00b12436f542daf2c95b8d9479c88525cbde
Mahmood Al-haddad <mahmoodal-haddad@mahmoods-mac.local>
2018-09-25 19:53:59 -0700

    Initial Commit

---
+++ HaloApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HaloApp
+//
+//  Created by Mahmood Al-haddad on 9/25/18.
+//  Copyright © 2018 Mahmood Al-haddad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HaloAppTests/HaloAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  HaloAppTests.swift
+//  HaloAppTests
+//
+//  Created by Mahmood Al-haddad on 9/25/18.
+//  Copyright © 2018 Mahmood Al-haddad. All rights reserved.
+//
+
+import XCTest
+@testable import HaloApp
+
+class HaloAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
